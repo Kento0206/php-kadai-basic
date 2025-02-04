ed6203a09cc853984086830135259d1588163c76
Kento0206 <ken200206@icloud.com>
2025-02-04 13:19:01 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>連想配列課題</title>
+</head>
+<body>
+    <p>
+        <?php
+            // 連想配列を変数に代入
+            $onion_info = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+            // 代入した変数の出力
+            print_r($onion_info);
+        ?>
+    </p>
+</body>
+</html>
